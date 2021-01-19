e96cadc1323173b2f221d4ed0c94b6d4923cfae4
Sebastien Lebreton <sebleb@microsoft.com>
2021-01-19 15:30:17 -0800

    VS Test Runner support (#42)
    * Use typecache and remove legacy callbacks
    
    * Restore legacy callbacks, and use TypeCache for them
    
    * Prototype test support
    
    * Prototype for VS test runner
    
    * Remove unused
    
    * Properly handle multiple TCP clients for our messaging system
    
    * Use a simple object model to communicate
    
    * Serialize more properties from results
    
    * Parse test mode when running tests
    
    * Adjust protocol format
    
    * Add type info to TestAdaptor
    
    * Extract types
    
    * Add ShowUsage to the protocol actions (#83)
    
    Co-authored-by: Sebastien Lebreton <sebastien@lebreton.fr>
    Co-authored-by: Jb Evain <jbevain@microsoft.com>
    Co-authored-by: Jb Evain <jb@evain.net>

---
+++ Packages/com.unity.ide.visualstudio/Editor/Testing/TestResultAdaptor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using UnityEditor.TestTools.TestRunner.Api;
+
+namespace Microsoft.Unity.VisualStudio.Editor.Testing
+{
+	internal class TestResultAdaptor
+	{
+		private ITestResultAdaptor _testResultAdaptor;
+		private TestResultAdaptor[] _children;
+
+		public string Name => _testResultAdaptor.Name;
+		public string FullName => _testResultAdaptor.FullName;
+
+		public int PassCount => _testResultAdaptor.PassCount;
+		public int FailCount => _testResultAdaptor.FailCount;
+		public int InconclusiveCount => _testResultAdaptor.InconclusiveCount;
+		public int SkipCount => _testResultAdaptor.SkipCount;
+
+		public string ResultState => _testResultAdaptor.ResultState;
+		public string StackTrace => _testResultAdaptor.StackTrace;
+
+		public TestStatusAdaptor TestStatus
+		{
+			get
+			{
+				switch (_testResultAdaptor.TestStatus)
+				{
+					case UnityEditor.TestTools.TestRunner.Api.TestStatus.Passed:
+						return TestStatusAdaptor.Passed;
+					case UnityEditor.TestTools.TestRunner.Api.TestStatus.Skipped:
+						return TestStatusAdaptor.Skipped;
+					case UnityEditor.TestTools.TestRunner.Api.TestStatus.Inconclusive:
+						return TestStatusAdaptor.Inconclusive;
+					case UnityEditor.TestTools.TestRunner.Api.TestStatus.Failed:
+						return TestStatusAdaptor.Failed;
+				}
+
+				throw new NotSupportedException();
+			}
+		}
+
+
+		public TestResultAdaptor[] Children
+		{
+			get
+			{
+				if (_children == null)
+				{
+					_children = _testResultAdaptor.Children.Select(ta => new TestResultAdaptor(ta)).ToArray();
+				}
+
+				return _children;
+			}
+		}
+
+		public TestResultAdaptor(ITestResultAdaptor testResultAdaptor)
+		{
+			_testResultAdaptor = testResultAdaptor;
+		}
+	}
+}
---
+++ Packages/com.unity.ide.visualstudio/Editor/Testing/TestRunnerCallbacks.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Newtonsoft.Json;
+using UnityEditor.TestTools.TestRunner.Api;
+
+namespace Microsoft.Unity.VisualStudio.Editor.Testing
+{
+	internal class TestRunnerCallbacks : ICallbacks
+	{
+		private JsonSerializerSettings _settings;
+
+		public TestRunnerCallbacks()
+		{
+			_settings = new JsonSerializerSettings
+			{
+			};
+		}
+
+		private string Serialize(object obj)
+		{
+			return JsonConvert.SerializeObject(obj, _settings);
+		}
+
+		public void RunFinished(ITestResultAdaptor testResultAdaptor)
+		{
+			VisualStudioIntegration.BroadcastMessage(Messaging.MessageType.RunFinished, Serialize(new TestResultAdaptor(testResultAdaptor)));
+		}
+
+		public void RunStarted(ITestAdaptor testAdaptor)
+		{
+			VisualStudioIntegration.BroadcastMessage(Messaging.MessageType.RunStarted, Serialize(new TestAdaptor(testAdaptor)));
+		}
+
+		public void TestFinished(ITestResultAdaptor testResultAdaptor)
+		{
+			VisualStudioIntegration.BroadcastMessage(Messaging.MessageType.TestFinished, Serialize(new TestResultAdaptor(testResultAdaptor)));
+		}
+
+		public void TestStarted(ITestAdaptor testAdaptor)
+		{
+			VisualStudioIntegration.BroadcastMessage(Messaging.MessageType.TestStarted, Serialize(new TestAdaptor(testAdaptor)));
+		}
+
+		private static string TestModeName(TestMode testMode)
+		{
+			switch (testMode)
+			{
+				case TestMode.EditMode: return "EditMode";
+				case TestMode.PlayMode: return "PlayMode";
+			}
+
+			throw new ArgumentOutOfRangeException();
+		}
+
+
+		internal void TestListRetrieved(TestMode testMode, ITestAdaptor testAdaptor)
+		{
+			// TestListRetrieved format:
+			// TestMode:Json
+
+			var value = TestModeName(testMode) + ":" + Serialize(new TestAdaptor(testAdaptor));
+			VisualStudioIntegration.BroadcastMessage(Messaging.MessageType.TestListRetrieved, value);
+		}
+	}
+}
