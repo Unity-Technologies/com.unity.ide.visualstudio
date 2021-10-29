f82e7b223c61baf88980089cb127f20fbee7fb1c
Simon Ferquel <simonferquel@simonferquel-1wdm.home>
2021-10-29 16:42:16 +0200

    Fix copy apple event integration copy
    Signed-off-by: Simon Ferquel <simonferquel@simonferquel-1wdm.home>

--- .scripts/build-appleeventintegration.sh
+++ .scripts/build-appleeventintegration.sh
@@ -2,5 +2,5 @@
 olddir="$(pwd)"
 cd "$(dirname $0)/../Packages/com.unity.ide.visualstudio/Editor/AppleEventIntegration~"
 xcodebuild -configuration Release
-mv build/Release/AppleEventIntegration.bundle ../Plugins/
+cp -R build/Release/AppleEventIntegration.bundle/* ../Plugins/AppleEventIntegration.bundle/
 cd "$olddir"
